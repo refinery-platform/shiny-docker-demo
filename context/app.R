3f283809410beec0a6f2ed106fb09ea8b514e695
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-05-08 10:42:11 -0400

    R makes reading from a url harder than it should be

--- context/app.R
+++ context/app.R
@@ -1,12 +1,13 @@
 library(shiny)
 library(rjson)
 
-input_data = fromJSON(Sys.getenv('INPUT_JSON_URL'))
+input_json = paste(readLines(Sys.getenv('INPUT_JSON_URL')), collapse = "")
+input_data = fromJSON(input_json)
 
 ui <- fluidPage(
     titlePanel("Hello World!"),
     mainPanel(paste("If this were for real, you would read from: ",
-              input_data['file_relationships']))
+                    input_data['file_relationships']))
 )
 
 server <- function(input, output) {}
